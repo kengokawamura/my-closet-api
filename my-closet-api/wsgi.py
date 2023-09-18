ee1ea4c71b8af1e99cc52ac19dd7ce49859ce7d6
kkawamura <kengokawamura0@gmail.com>
2023-09-18 16:14:02 +0900

    first table

--- my-closet-api/wsgi.py
+++ my-closet-api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for my-close-api project.
+WSGI config for my-closet-api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my-close-api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my-closet-api.settings')
 
 application = get_wsgi_application()
