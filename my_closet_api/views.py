ffe2018c889df281b46789189d0bd1dbe29014ba
kkawamura <kengokawamura0@gmail.com>
2023-11-25 21:38:01 +0900

    test

--- my_closet_api/views.py
+++ my_closet_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from datetime import datetime
+from .models import category
 
 # Create your views here.
+
+def create_category(request):
+    category.objects.create(created_at=datetime.now(), updated_at=datetime.now(), name='test_1')
+    return HttpResponse("category successfully saved.")
